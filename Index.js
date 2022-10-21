3661998252bf871310d07ab93df35b9f68b669df
Himel Hasan <himelhasan497@gmail.com>
2022-10-21 13:11:35 +0600

    api updated to get news and category data acoording to id

--- Index.js
+++ Index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require("./Data/categories");
+const categories = require("./Data/categories.json");
+const news = require("./Data/news.json");
 
 app.get("/", (req, res) => {
   res.send("hurray ! news api running");
@@ -13,6 +14,26 @@ app.get("/", (req, res) => {
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/news/:id", (req, res) => {
+  console.log(req.params.id);
+  const id = req.params.id;
+  const selectedNews = news.find((singleNews) => singleNews._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (id == 08) {
+    res.send(news);
+  } else {
+    const selectedCategory = news.filter(
+      (singleCategory) => singleCategory.category_id === id
+    );
+    res.send(selectedCategory);
+  }
 });
 
 app.listen(port, () => {
